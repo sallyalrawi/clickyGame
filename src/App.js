24b66c247ba0607478d804e605b4751576e9210e
sally <sallyakrum83@gmail.com>
2019-04-24 21:43:55 -0500

    fixing stuff

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {/* these components are going to be inside the src folder every componant are going to be a js file */}
+    {/* slavic explaination */}
+    {/* <Header /> */}
+      {/* <ImagesContainer> */}
+        {/* array of images goes here */}
+        {/* <Card image=""/> */}
+      {/* </ImagesContainer> */}
+    {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
